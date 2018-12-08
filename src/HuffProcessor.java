c90ded279368ba9f9c9d2916134d5eceaeea67db
Amr Bedawi <amr.bedawi@duke.edu>
2018-12-08 00:41:15 -0500

    d

--- src/HuffProcessor.java
+++ src/HuffProcessor.java
@@ -129,7 +129,16 @@ public class HuffProcessor {
 
 	private void writeCompressedBits(String[] codings, BitInputStream in, BitOutputStream out) {
 		// TODO Auto-generated method stub
-		
+		int bits = 0;
+		String code;
+		while(true) {
+			if(bits == -1) break; 
+			bits = in.readBits(BITS_PER_WORD);
+			code = codings[bits];
+			out.writeBits(code.length(), Integer.parseInt(code,2));
+		}
+		code = codings[PSEUDO_EOF];
+		out.writeBits(code.length(), Integer.parseInt(code,2));
 	}
 
 	/**
